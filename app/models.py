58164828a71f7f95ab1fd6dbe1348349358407ca
Carlos Henrique <carlos.henrique@xmobots.com.br>
2024-10-30 11:00:55 -0300

    Adicionando nova tabela para pratica de migration

--- app/models.py
+++ app/models.py
@@ -9,3 +9,12 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'<Item {self.id}: {self.name}>'
+
+
+class Item2(db.Model):
+    __tablename__ = 'item2'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Item {self.id}: {self.name}>'
